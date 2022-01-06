b347d33089b4661d116958bff3779b980e75c7e3
Han <hander.juan@gmail.com>
2022-01-06 21:26:30 +0200

    migrate

--- categories/models.py
+++ categories/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-
+from cloudinary.models import CloudinaryField
 
 class Category(models.Model):
     title = models.CharField(max_length=100)
-    image = models.ImageField(upload_to='categories')
+    image = CloudinaryField('image')
 
     def __str__(self):
         return self.title
