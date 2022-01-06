5c98024d55b839b6918c36192fe23cefc7ccf319
Han <hander.juan@gmail.com>
2022-01-06 16:03:08 +0200

    static

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -147,9 +147,7 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': datetime.timedelta(days=1)
 }
 
-STATICFILES_DIRS = [
-    os.path.join('static'),
-]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
